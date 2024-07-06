51326e1e86736a17ca125647b4208458864e9c33
patrickpoole <patrickcpoole@gmail.com>
2024-07-05 23:23:01 -0600

    Revert "Merge pull request #17 from Patrickcpoole/main-branch/EJPM/friends-gui"
    This reverts commit 4bf863c57ff5c1748cdf3a6b9b669506234c73e6, reversing
    changes made to 9c104038b6cd77076f4ac0e28dd8e2af3d0eb612.

--- constants/icons.ts
+++ constants/icons.ts
@@ -7,10 +7,6 @@ import rewards from '../assets/icons/rewards-icon.png'
 import google from '../assets/icons/google-icon.png'
 import facebook from '../assets/icons/facebook-icon.png'
 import apple from '../assets/icons/apple-icon.png'
-import search from '../assets/icons/search-icon.png'
-import verified from '../assets/icons/verified-icon.png'
-import add from '../assets/icons/add-icon.png'
-import notifications from '../assets/icons/notifications-icon.png'
 
 export default {
   logo,
@@ -21,9 +17,5 @@ export default {
   rewards,
   google,
   facebook,
-  apple,
-  search,
-  verified,
-  add,
-  notifications
+  apple
 }
