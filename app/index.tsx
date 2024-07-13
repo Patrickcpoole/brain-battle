ce5484c0a9f02c08942246fc2f5060b3b5980606
patrickpoole <patrickcpoole@gmail.com>
2024-07-13 15:28:25 -0600

    refactor: organized code and removed not needed directories

--- app/index.tsx
+++ app/index.tsx
@@ -1,61 +1,78 @@
 import { View, Text, Image, TouchableOpacity, Alert } from 'react-native';
 import { scale, verticalScale, moderateScale } from 'react-native-size-matters';
-import React, { useState } from 'react';
-import { router } from 'expo-router';
+import React, { useState, useEffect } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Picker } from '@react-native-picker/picker';
 import images from '@/constants/images';
-
-import { Client, Account } from 'react-native-appwrite';
+import {
+	demoLogin,
+	checkForCurrentSession,
+	removeCurrentSessions,
+} from '@/lib/appwrite';
 import { project } from '../lib/app-details';
 
 const App = () => {
+	const [player, setPlayer] = useState('demouser1@gmail.com');
 
-    const client = new Client();
-    client.setEndpoint(project.endpoint);
-    client.setProject(project.id);
-    client.setPlatform(project.platform);
+	useEffect(() => {
+		const checkForAndRemoveSessions = async () => {
+			const currentSessions = await checkForCurrentSession();
+			console.log('current sessions', currentSessions);
+			if (currentSessions.length > 0) {
+                console.log('removing sessions')
+				removeCurrentSessions();
+			}
+		};
+		checkForAndRemoveSessions();
+	}, []);
 
-    const account = new Account(client);
-
-    const result = account.get()
-        .then((response) => {
-            console.log(response);
-            router.replace("/(demo)/ready");
-        })
-        .catch((exception) => (console.error(exception)));
-
-    const [player, setPlayer] = useState("demouser1@gmail.com");
-
-    function login() {
-        
-        console.log("Logged in with " + player);
-        const account = new Account(client);
-
-        const result = account.createEmailPasswordSession(player, "1234567890")
-            .then((response) => {
-                console.log(response);
-                router.replace("/(demo)/ready")
-            })
-            .catch((exception) => (Alert.alert("Warning!")));
-
-    }
-
-    return (
-        <SafeAreaView className="bg-light">
-            <View className="w-full h-full flex justify-center" style={{gap: verticalScale(30)}}>
-                <Image source={images.demoLogo} className="flex self-center" resizeMode="contain" style={{width: scale(300), height: verticalScale(100)}} />
-                <Picker selectedValue={player} onValueChange={(email) => (setPlayer(email))}>
-                    <Picker.Item label="Demo User 1" value={"demouser1@gmail.com"} style={{fontSize: moderateScale(14)}} />
-                    <Picker.Item label="Demo User 2" value={"demouser2@gmail.com"} style={{fontSize: moderateScale(14)}} />
-                </Picker>
-                <TouchableOpacity onPress={() => login} className="flex self-center bg-primary rounded-full" style={{width: scale(300), paddingVertical: verticalScale(12), marginBottom: verticalScale(30)}}>
-                    <Text className="font-clashsemibold text-center text-white" style={{fontSize: moderateScale(18)}}> Select User </Text>
-                </TouchableOpacity>
-            </View>
-        </SafeAreaView>
-    );
-
+	return (
+		<SafeAreaView className='bg-light'>
+			<View
+				className='w-full h-full flex justify-center'
+				style={{ gap: verticalScale(30) }}
+			>
+				<Image
+					source={images.demoLogo}
+					className='flex self-center'
+					resizeMode='contain'
+					style={{ width: scale(300), height: verticalScale(100) }}
+				/>
+				<Picker
+					selectedValue={player}
+					onValueChange={(email) => setPlayer(email)}
+				>
+					<Picker.Item
+						label='Demo User 1'
+						value={'demouser1@gmail.com'}
+						style={{ fontSize: moderateScale(14) }}
+					/>
+					<Picker.Item
+						label='Demo User 2'
+						value={'demouser2@gmail.com'}
+						style={{ fontSize: moderateScale(14) }}
+					/>
+				</Picker>
+				<TouchableOpacity
+					onPress={() => demoLogin(player)}
+					className='flex self-center bg-primary rounded-full'
+					style={{
+						width: scale(300),
+						paddingVertical: verticalScale(12),
+						marginBottom: verticalScale(30),
+					}}
+				>
+					<Text
+						className='font-clashsemibold text-center text-white'
+						style={{ fontSize: moderateScale(18) }}
+					>
+						{' '}
+						Select User{' '}
+					</Text>
+				</TouchableOpacity>
+			</View>
+		</SafeAreaView>
+	);
 };
 
 export default App;
